16cdc5425c4ed84d7a6a131582f9b42e8175b462
LeonardoC0316 <leo.cimitan@gmail.com>
2025-06-13 13:02:19 +0200

    Update cifrario_cesare.py

--- cifrario_cesare.py
+++ cifrario_cesare.py
@@ -1,10 +1,33 @@
-from string import ascii_lowercase
-def cifrario(s:str, k:int) -> str:
-    l:str = ""
+from string import ascii_lowercase, ascii_uppercase
 
+def crifrario(s: str, k: int) -> str:
+    l = ""
     for carattere in s:
-        index: int = ascii_lowercase.index(carattere)
-        index = (index + k) % len(ascii_lowercase)
-        l += ascii_lowercase[index]
+        if carattere in ascii_lowercase:
+            index = (ascii_lowercase.index(carattere) + k) % len(ascii_lowercase)
+            l += ascii_lowercase[index]
+        elif carattere in ascii_uppercase:
+            index = (ascii_uppercase.index(carattere) + k) % len(ascii_uppercase)
+            l += ascii_uppercase[index]
+        else:
+            l += carattere 
     return l
 
+def decifrazione(s: str, k: int) -> str:
+    l = ""
+    for carattere in s:
+        if carattere in ascii_lowercase:
+            index = (ascii_lowercase.index(carattere) - k) % len(ascii_lowercase)
+            l += ascii_lowercase[index]
+        elif carattere in ascii_uppercase:
+            index = (ascii_uppercase.index(carattere) - k) % len(ascii_uppercase)
+            l += ascii_uppercase[index]
+        else:
+            l += carattere 
+    return l
+
+testo_cifrato = crifrario("Ciao mi chiamo Leonardo", 7)
+print("Testo cifrato:", testo_cifrato)
+
+testo_decifrato = decifrazione(testo_cifrato, 7)
+print("Testo decifrato:", testo_decifrato)
